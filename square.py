9d22dbcd8b524a4cb924e1d442837290c2d926dd
sreep <pavansai5010@gmail.com>
2025-01-05 13:59:54 +0530

    initial commit

---
+++ square.py
@@ -0,0 +1,10 @@
+# get squares between start and end numbers
+start = int(input("Enter yout starting number: "))
+end = int(input("Enter yout ending number: "))
+def get_sqrs(start,end):
+    print(f"square between {start} and {end} numbers")
+    while start<=end:
+        print(start**2)
+        start+=1
+
+get_sqrs(start,end)
