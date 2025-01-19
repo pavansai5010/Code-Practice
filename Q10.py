aac4856bb3a3992212ace3a671163df08dd6da4f
sreep <pavansai5010@gmail.com>
2025-01-19 16:52:20 +0530

    modified one

--- Q10.py
+++ Q10.py
@@ -1,1 +1,8 @@
-print(4%10)
+num = int(input("enter a number: "))
+product = 1
+no = num
+def product_of_digits(num):
+    while no != 0 :
+        r = no%10
+        product = product * r
+        no = no // 10
