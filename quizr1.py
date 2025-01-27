e017191bb9f83568d86dc02dcc5bc968668bce42
sreep <pavansai5010@gmail.com>
2025-01-27 23:37:49 +0530

    updated commit

--- quizr1.py
+++ quizr1.py
@@ -1,7 +1,11 @@
+'''
+to get 2nd largest number form the given input without using built in functions
+'''
+
 n=int(input(":"))
 elements=input("enter elements separated by space: ")
 lst=[]
-for i in (elements.split(" ")):
+for i in (elements.split(" ")): # This for loop is to remove the duplicate elements for the given input
     if i not in lst:
         lst.append(i)
 print
@@ -9,9 +13,9 @@ l=len(lst)
 for i in range(l):
     for j in range(i+1,l):
         if lst[i] < lst[j]:
-            lst[i],lst[j] = lst[j],lst[i]
+            lst[i],lst[j] = lst[j],lst[i] # this swaps the element if current element is less than 2nd iteration element
 print(lst)
-print(lst[1])
+print(lst[1]) # this prints the 2nd largest element
 
 
 
