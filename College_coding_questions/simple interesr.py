1436327113d76d0900d537fae260950ae5716773
sreep <pavansai5010@gmail.com>
2025-02-06 07:27:58 +0530

    initial commit

---
+++ College_coding_questions/simple interesr.py
@@ -0,0 +1,7 @@
+p = int(input("Enter the principle amount: "))
+t = int(input("Enter the time period: "))
+r = int(input("Enter the rate of interest per annum: "))
+def si(p,t,r):
+    print("simple intrest: ",(p*t*r)/100)
+print(si(p,t,r))
+    
