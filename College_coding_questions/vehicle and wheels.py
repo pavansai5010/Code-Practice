b9acafd37ea40660cc40c62fd00bc4d6f3538fc2
sreep <pavansai5010@gmail.com>
2025-02-24 23:04:50 +0530

    updated commit

---
+++ College_coding_questions/vehicle and wheels.py
@@ -0,0 +1,5 @@
+v = int(input("enter no of vehicles: "))
+w = int(input("enter no of wheels: "))
+v1 = ((4*v)-w)/2
+v2 = v-v1
+print(f"two weels vehicles are {int(v1)} and four wheels vehilces are {int(v2)}")
